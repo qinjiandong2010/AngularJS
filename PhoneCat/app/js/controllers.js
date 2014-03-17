4693b9b9b5c5e4e4a14a4da7ef091fe61a8ef5c0
unknown <qinjiandong2010@gmail.com>
2014-03-17 14:03:34 +0800

    PhoneCat demo

---
+++ PhoneCat/app/js/controllers.js
@@ -0,0 +1,22 @@
+var phonecatApp = angular.module("phonecatApp",[]);
+
+phonecatApp.controller("PhoneListCtrl",function($scope){
+  $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+  $scope.orderProp="name";
+});
+
+phonecatApp.controller("HelloCtrl",function($scope){
+	$scope.greeting = {
+		"text":"Hello"
+	};
+	$scope.showGreeting = function(){
+		alert($scope.greeting.text);
+	};
+});
